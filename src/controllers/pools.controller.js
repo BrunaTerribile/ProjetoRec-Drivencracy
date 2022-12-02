2f7f2a34cf5855f21589c9a09adad39fc34a22ee
Bruna Terribile <brunaterribile@gmail.com>
2022-12-01 22:35:40 -0300

    Feat: /pool - createPool + poolSchema

--- src/controllers/pools.controller.js
+++ src/controllers/pools.controller.js
@@ -1,5 +1,24 @@
+import { pollsCollection } from "../database/db.js"
+import { poolsSchema } from "../models/pools.model.js";
 
 export async function createPool(req, res) {
+    const pool = req.body;
+
+    try {
+        const { error } = poolsSchema.validate(pool, { abortEarly: false });
+
+        if (error) {
+          const errors = error.details.map((detail) => detail.message);
+          return res.status(400).send(errors);
+        }
+    
+        await pollsCollection.insertOne(pool);
+    
+        res.status(201).send("Enquete criada com sucesso!");
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
 }
 
 export async function getPools(req, res) {
